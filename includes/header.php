db64f40419ccca013681f33cbd8f6312c1d62c52
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-15 13:00:14 +0200

    header della pagina con style.css per la grafica

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<?php
+// Includi il file di sessione
+// Mostra intestazione HTML con nome utente e numero operazioni
+// Includi link alla home e al form di aggiunta
+
+require 'includes/session.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gestione Eventi</title>
+</head>
+<body>
